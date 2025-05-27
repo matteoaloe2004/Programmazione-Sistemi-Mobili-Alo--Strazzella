ed752c2bc17796c3409c323f35642f444803e9ef
eliastrazzella <elia.strazzella@studio.unibo.it>
2025-05-27 16:21:37 +0200

    Update_Map_2.0

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/trovaparco/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.trovaparco.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.trovaparco.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String MAPS_API_KEY = "AIzaSyBhHTO8zvllwkHhsW0_JlMVoAJ2ewbHNFU";
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/trovaparco/MainActivity.java
@@ -0,0 +1,16 @@
+package com.trovaparco;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0007\u001a\u00020\b2\b\u0010\t\u001a\u0004\u0018\u00010\nH\u0014R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000b"}, d2 = {"Lcom/trovaparco/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "drawerLayout", "Landroidx/drawerlayout/widget/DrawerLayout;", "viewModel", "Lcom/trovaparco/viewmodel/MapViewModel;", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+    private androidx.drawerlayout.widget.DrawerLayout drawerLayout;
+    private com.trovaparco.viewmodel.MapViewModel viewModel;
+    
+    public MainActivity() {
+        super();
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+}
